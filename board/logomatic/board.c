eaf53cdfc41b124a1f7888e259a73954ccca7abc
Ingo Albrecht <prom@berlin.ccc.de>
2011-06-10 07:48:32 +0200

    Board cleanup.

--- board/logomatic/board.c
+++ board/logomatic/board.c
@@ -3,6 +3,7 @@
 
 #include <core/timer.h>
 
+#include <lpc/scb.h>
 #include <lpc/vic.h>
 #include <lpc/pll.h>
 #include <lpc/mam.h>
@@ -16,88 +17,23 @@
 #include <lpc/timer.h>
 #include <lpc/pinsel.h>
 
-#define GPIO_LED_STAT1 11
-#define GPIO_LED_STAT2 2
-
-#define INTV_STOP     0
-#define	INTV_USB	  1
-#define INTV_UART0    2
-#define INTV_UART1    3
-#define INTV_TIMER0   4
-#define INTV_TIMER1   5
-
-DEFINE_EINT_VECTOR(eint1_vector, EINT1);
-DEFINE_UART_VECTOR(uart0_vector, 0);
-DEFINE_UART_VECTOR(uart1_vector, 1);
-DEFINE_TIMER_VECTOR(timer0_vector, 0);
-DEFINE_TIMER_VECTOR(timer1_vector, 1);
-
-
-static void timer_tick (always_unused int t, always_unused timer_match_t m)
+/* Clocking */
+static void clock_init(void)
 {
-	tick_handler();
+	/* boot pll and set full throttle on various things */
+	pll_configure(PLL_CORE, &pll_60mhz);
+	pll_lock_blocking(PLL_CORE);
+	pll_connect(PLL_CORE);
 }
 
+/* Timing interrupt vectors (XXX configuration buggy) */
+DEFINE_TIMER_VECTOR(timer0_vector, 0); // XXX configuration dependent
+DEFINE_TIMER_VECTOR(timer1_vector, 1); // XXX configuration dependent
 
-static void led_init(void)
-{
-	gpio_pin_high(GPIO_LED_STAT1);
-	gpio_pin_high(GPIO_LED_STAT2);
-
-	gpio_pin_set_direction(GPIO_LED_STAT1, 1);
-	gpio_pin_set_direction(GPIO_LED_STAT2, 1);
-}
-
-void led_stat1(bool_t lit)
-{
-	if(lit) {
-		gpio_pin_low(GPIO_LED_STAT1);
-	} else {
-		gpio_pin_high(GPIO_LED_STAT1);
-	}
-}
-
-void led_stat2(bool_t lit)
-{
-	if(lit) {
-		gpio_pin_low(GPIO_LED_STAT2);
-	} else {
-		gpio_pin_high(GPIO_LED_STAT2);
-	}
-}
-
-
-stop_handler_t the_stop_handler;
-
-void set_stop_handler(stop_handler_t handler)
-{
-	the_stop_handler = handler;
-}
-
-static void stop_handler(int eint)
-{
-	led_stat2(1);
-
-	if(the_stop_handler) {
-		the_stop_handler();
-	}
-}
-
-
-void heartbeat_function (always_unused struct timer *t, nanosecs_t now)
-{
-	nanosecs_t second = now / NANOSECS_SEC;
-	bool_t odd = second & 1 ? BOOL_TRUE : BOOL_FALSE;
-
-	led_stat1(odd);
-}
-
-DEFINE_PERIODIC_TIMER(heartbeat, heartbeat_function, 0.5 * NANOSECS_SEC);
-
-
+/* Clock device */
 static nanosecs_t clktimer_read (struct clock_device *clock)
 {
-	return timer_read(TIMER1) * NANOSECS_USEC;
+	return timer_read(LPC_CLOCK_TIMER) * NANOSECS_USEC;
 }
 
 struct clock_device clktimer = {
@@ -107,11 +43,7 @@ struct clock_device clktimer = {
 	.read = &clktimer_read,
 };
 
-
-
-#define LPC_TIMER TIMER0
-#define LPC_TIMER_MR MR0
-
+/* Timer device */
 static int lpc_timer_initialize (struct timer_device *dev);
 static int lpc_timer_program (struct timer_device *dev, int mode, nanosecs_t delta);
 
@@ -177,53 +109,95 @@ static int lpc_timer_program (struct timer_device *dev, int mode, nanosecs_t del
 	return 0;
 }
 
-
-
-void board_early_init(void)
+static void time_init(void)
 {
-	scb_init();
-
-	// enable status leds early for debugging
-	led_init();
-
-	// boot pll and set full throttle on various things
-	pll_configure(PLL_CORE, &pll_60mhz);
-	pll_lock_blocking(PLL_CORE);
-	pll_connect(PLL_CORE);
-
-	// initialize additional busses and memories
-	mam_init();
-	vpb_init();
-
-	// initialize the real time clock
+	/* real time clock */
 	rtc_init();
 
-	// STOP button
+	/* TIMER0 as system timer */
+	device_add(&lpc_timer.dev);
+	vic_configure(INTV_TIMER0, INT_TIMER0, &timer0_vector);
+	vic_enable(INT_TIMER0);
+
+	/* TIMER1 as clock timer */
+	timer_init(TIMER1);
+	timer_prescale(TIMER1, 60);
+	timer_enable(TIMER1, BOOL_TRUE);
+	device_add(&clktimer.dev);
+}
+
+
+/* LEDs */
+static void leds_init(void)
+{
+	gpio_pin_high(GPIO_LED_STAT1);
+	gpio_pin_high(GPIO_LED_STAT2);
+
+	gpio_pin_set_direction(GPIO_LED_STAT1, 1);
+	gpio_pin_set_direction(GPIO_LED_STAT2, 1);
+}
+
+void led_stat1(bool_t lit)
+{
+	if(lit) {
+		gpio_pin_low(GPIO_LED_STAT1);
+	} else {
+		gpio_pin_high(GPIO_LED_STAT1);
+	}
+}
+
+void led_stat2(bool_t lit)
+{
+	if(lit) {
+		gpio_pin_low(GPIO_LED_STAT2);
+	} else {
+		gpio_pin_high(GPIO_LED_STAT2);
+	}
+}
+
+/* STOP button */
+DEFINE_EINT_VECTOR(eint1_vector, EINT1);
+
+stop_handler_t the_stop_handler;
+
+void set_stop_handler(stop_handler_t handler)
+{
+	the_stop_handler = handler;
+}
+
+static void stop_handler(int eint)
+{
+	led_stat2(1);
+
+	if(the_stop_handler) {
+		the_stop_handler();
+	}
+}
+
+static void stop_init(void)
+{
 	pin_set_function(PIN3, PIN_FUNCTION_EINT1);
 	eint_configure(EINT1, 1, 0);
 	eint_handler(EINT1, &stop_handler);
 	vic_configure(INTV_STOP, INT_EINT1, &eint1_vector);
 	vic_enable(INT_EINT1);
+}
 
-	// TIMER0 as system timer
-	device_add(&lpc_timer.dev);
-	vic_configure(INTV_TIMER0, INT_TIMER0, &timer0_vector);
-	vic_enable(INT_TIMER0);
 
-	// TIMER1 as clock timer
-	timer_init(TIMER1);
-	timer_prescale(TIMER1, 60);
-	timer_enable(TIMER1, BOOL_TRUE);
-	device_add(&clktimer.dev);
+/* UARTs */
+DEFINE_UART_VECTOR(uart0_vector, 0);
+DEFINE_UART_VECTOR(uart1_vector, 1);
 
-	// UART0
+static void uarts_init(void)
+{
+	/* UART0 */
 	pin_set_function(PIN0, PIN_FUNCTION_UART0_TXD);
 	pin_set_function(PIN1, PIN_FUNCTION_UART0_RXD);
 	uart_init(0, 38400);
 	vic_configure(INTV_UART0, INT_UART0, &uart0_vector);
 	vic_enable(INT_UART0);
 
-	// UART1
+	/* UART1 */
 	pin_set_function(PIN8, PIN_FUNCTION_UART1_TXD);
 	pin_set_function(PIN9, PIN_FUNCTION_UART1_RXD);
 	uart_init(1, 9600);
@@ -231,7 +205,39 @@ void board_early_init(void)
 	vic_enable(INT_UART1);
 }
 
+/* heartbeat blink on STAT1 */
+void heartbeat_function (always_unused struct timer *t, nanosecs_t now)
+{
+	nanosecs_t second = now / NANOSECS_SEC;
+	bool_t odd = second & 1 ? BOOL_TRUE : BOOL_FALSE;
+
+	led_stat1(odd);
+}
+
+DEFINE_PERIODIC_TIMER(heartbeat, heartbeat_function, 0.5 * NANOSECS_SEC);
+
+
+/* Board initialization */
+void board_early_init(void)
+{
+	/* enable status leds early for debugging */
+	leds_init();
+	/* then configure system clocks */
+	clock_init();
+	/* initialize additional busses and memories */
+	scb_init();
+	mam_init();
+	vpb_init();
+	/* initialize timing devices */
+	time_init();
+	/* initialize stop button */
+	stop_init();
+	/* initialize UARTs */
+	uarts_init();
+}
+
 void board_init(void)
 {
+	/* start the periodic heartbeat timer */
 	START_PERIODIC_TIMER(heartbeat);
 }
--- board/logomatic/board.h
+++ board/logomatic/board.h
@@ -3,6 +3,21 @@
 
 #include <core/types.h>
 
+#define INTV_STOP     0
+#define	INTV_USB	  1
+#define INTV_UART0    2
+#define INTV_UART1    3
+#define INTV_TIMER0   4
+#define INTV_TIMER1   5
+
+#define LPC_CLOCK_TIMER TIMER1
+
+#define LPC_TIMER TIMER0
+#define LPC_TIMER_MR MR0
+
+#define GPIO_LED_STAT1 11
+#define GPIO_LED_STAT2 2
+
 void board_early_init(void);
 void board_init(void);
 
